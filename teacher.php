8f6d79ab0dee1736371ce5de37a1b76cfb0b3147
wwajiha <wwajiha729@gmail.com>
2023-01-10 00:52:10 +0500

    teacher  table changes

--- teacher.php
+++ teacher.php
@@ -13,18 +13,18 @@
     <form method="post">
     
     <div class="form-group">
-    <label> Teacher Name</label>
+    <label> Teacher Name wajeeha </label>
     <input type="text" name="name" class="form-control" required>
     </div>
     
      <div class="form-group">
     <label>Contact</label>
-    <input type="text" name="address" class="form-control" required>
+    <input type="text" name="contact" class="form-control" required>
     </div>    
         
      <div class="form-group">
     <label> Address</label>
-    <input type="text" name="contact" class="form-control" required>
+    <input type="text" name="adress2" class="form-control" required>
     </div>    
         
         
@@ -51,6 +51,13 @@
        ?>
     </select>
     </div>  
+
+
+    <div class="form-group">
+    <label>Contact School</label>
+    <input type="text" name="wajeeha" class="form-control" required>
+    </div>    
+     
         
         <center> <input type="submit" name="sub" value="Add" class="btn ">  </center>
         <br>    
@@ -64,11 +71,14 @@
 
 if(isset($_POST['sub'])){
     $name = $_POST['name'];
-    $addrss= $_POST['address'];
+    $addrss= $_POST['adress2'];
     $contact = $_POST['contact'];
     $school = $_POST['school'];
+    $location7= 'xyz';
+    $abc= '';
     
-    $query = "insert into teacher(name , address, contact , school_id) values('$name' , '$addrss', '$contact' , '$school')";
+    
+    $query = "insert into teacher(name , address, contact , school_id,location_home_town) values('$name' , '$addrss', '$contact' , '$school','$location7')";
     $result = mysqli_query($connection , $query);
     
     if(!$result){
